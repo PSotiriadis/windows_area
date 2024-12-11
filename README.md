72e9f0e9f136943719adeb0890fc18eec860c9d2
Prodromos Sotiriadis <sotiriadis.prodromos@unibw.de>
2024-12-11 14:05:00 +0100

    Improve ListInstallSoftware.ps1 script

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # windows_scripts
 
-This repository is a container for various windows scripts.
+This repository is a container for various windows features.
 
+
+## Features
+### Scripts:
 Below will listed the scripts on this repository:
 1. ListInstallSoftware.ps1
 2.
+
+#### ExcelMacros: 
+Below will be lested the Excel Macros in this repository:
+1. RemoveDuplicateRows
+2. TrimSpacesInSelectedCells
--- WindowsScripts/ListInstallSoftware.ps1
+++ WindowsScripts/ListInstallSoftware.ps1
@@ -1,11 +1,29 @@
 # Retrieve all installed software properties
 $installedSoftware = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*", "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
 
+
 # Filter out entries where DisplayName or Publisher is empty or null
 $filteredSoftware = $installedSoftware | Where-Object { 
     $_.DisplayName -and $_.Publisher 
 }
 
+# Retrieve the machine's IP address
+$ipAddress = (Test-Connection -ComputerName (hostname) -Count 1).IPv4Address.IPAddressToString
+
+# Extract the last octet of the IP address
+$lastOctet = $ipAddress.Split('.')[-1]
+
+# Construct the export file name using the last octet
+$exportFileNameBase = "installed_software_PC$lastOctet"
+
+# Full paths for the export files
+$csvFilePath = "C:\Users\sotiriadis\Documents\$exportFileNameBase.csv"
+$txtFilePath = "C:\Users\sotiriadis\Documents\$exportFileNameBase.txt"
+
+
 # Export the data to a CSV file with a semicolon delimiter
 $filteredSoftware | Select-Object DisplayName, DisplayVersion, Publisher | 
-    Export-Csv -Path "C:\Users\sotiriadis\Documents\install_software.csv" -NoTypeInformation -Force -Delimiter ";"
+    Export-Csv -Path $csvFilePath -NoTypeInformation -Force -Delimiter ";"
+
+# Export the data to a CSV file with a semicolon delimiter
+$filteredSoftware | Select-Object DisplayName, DisplayVersion, Publisher | Out-File -FilePath $txtFilePath -Force
